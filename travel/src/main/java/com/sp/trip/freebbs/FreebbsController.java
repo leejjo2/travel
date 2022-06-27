96bc618981f7ec47535cc88bd71adee484b844bb
user <user@DESKTOP-DB9U1HD>
2022-06-27 18:33:37 +0900

    강예찬 - 자유게시판완료

--- travel/src/main/java/com/sp/trip/freebbs/FreebbsController.java
+++ travel/src/main/java/com/sp/trip/freebbs/FreebbsController.java
@@ -18,6 +18,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sp.trip.common.FileManager;
 import com.sp.trip.common.MyUtil;
@@ -129,6 +130,156 @@ public class FreebbsController {
 			}
 			return "redirect:/freebbs/list";
 			
+		}
+		
+		@RequestMapping(value = "article", method = RequestMethod.GET)
+		public String article(@RequestParam int freeNum,
+				@RequestParam String page,
+				@RequestParam(defaultValue = "all") String condition,
+				@RequestParam(defaultValue = "") String keyword,
+				HttpSession session,
+				Model model) throws Exception {
+			
+			SessionInfo info = (SessionInfo) session.getAttribute("member");
+			keyword = URLDecoder.decode(keyword, "utf-8");
+			
+			String query = "page=" + page;
+			if(keyword.length() !=0) {
+				query += "&condition=" + condition + "&keyword=" + URLEncoder.encode(keyword, "UTF-8");
+				
+			}
+			
+			Board dto = service.readBoard(freeNum);
+			if(dto == null) {
+				return "redirect:/freebbs/list?" + query;
+			}
+			if (!dto.getUserId().equals(info.getUserId()))
+				return "redirect:/";
+
+			dto.setContent(dto.getContent().replaceAll("\n", "<br>"));
+			
+			// 이전 글, 다음 글
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("userId", info.getUserId());
+			map.put("condition", condition);
+			map.put("keyword", keyword);
+			map.put("freeNum", freeNum);
+			
+			Board preReadDto = service.preReadBoard(map);
+			Board nextReadDto = service.nextReadBoard(map);
+			
+			List<Board> listFile = service.listFile(freeNum);
+			
+			model.addAttribute("dto", dto);
+			model.addAttribute("preReadDto", preReadDto);
+			model.addAttribute("nextReadDto", nextReadDto);
+			model.addAttribute("listFile", listFile);
+
+			model.addAttribute("page", page);
+			model.addAttribute("query", query);
+			
+			return ".freebbs.freebbsarticle";
+		}
+		
+		@RequestMapping(value = "update", method = RequestMethod.GET)
+		public String updateForm(@RequestParam int freeNum,
+				@RequestParam String page,
+				HttpSession session,
+				Model model) throws Exception {
+			
+			SessionInfo info = (SessionInfo) session.getAttribute("member");
+			
+			Board dto = service.readBoard(freeNum);
+			
+			if (dto == null) {
+				return "redirect:/freebbs/list?page=" + page;
+			}
+			
+			if (! dto.getUserId().equals(info.getUserId())) {
+				return "redirect:/";
+			}
+			
+			List<Board> listFile = service.listFile(freeNum);
+			
+			model.addAttribute("dto", dto);
+			model.addAttribute("listFile", listFile);
+
+			model.addAttribute("page", page);
+			model.addAttribute("mode", "update");
+			
+			return ".freebbs.write";
+		}
+		
+		@RequestMapping(value = "update", method = RequestMethod.POST)
+		public String updateSubmit(Board dto,
+				@RequestParam String page,
+				HttpSession session) throws Exception {
+			String root = session.getServletContext().getRealPath("/");
+			String pathname = root + "uploads" + File.separator + "Board";
+
+			try {
+				service.updateBoard(dto, pathname);
+			} catch (Exception e) {
+			}
+
+			return "redirect:/album/article?num=" + dto.getFreeNum() + "&page=" + page;
+		}
+		
+		@RequestMapping(value = "delete", method = RequestMethod.GET)
+		public String delete(@RequestParam int freeNum,
+				@RequestParam String page,
+				@RequestParam(defaultValue = "all") String condition,
+				@RequestParam(defaultValue = "") String keyword,
+				HttpSession session) throws Exception {
+
+			keyword = URLDecoder.decode(keyword, "utf-8");
+			String query = "page=" + page;
+			if (keyword.length() != 0) {
+				query += "&condition=" + condition + "&keyword=" + URLEncoder.encode(keyword, "UTF-8");
+			}
+
+			String root = session.getServletContext().getRealPath("/");
+			String pathname = root + "uploads" + File.separator + "Board";
+
+			SessionInfo info = (SessionInfo) session.getAttribute("member");
+			Board dto = service.readBoard(freeNum);
+			if (dto == null) {
+				return "redirect:/freebbs/list?page=" + page;
+			}
+
+			if (!dto.getUserId().equals(info.getUserId())) {
+				return "redirect:/";
+			}
+
+			try {
+				service.deleteBoard(freeNum, pathname);
+			} catch (Exception e) {
+			}
+
+			return "redirect:/freebbs/list?" + query;
+		}
+
+		@RequestMapping(value = "deleteFile", method = RequestMethod.POST)
+		@ResponseBody
+		public Map<String, Object> deleteFile(@RequestParam int freeFileNum, HttpSession session) throws Exception {
+
+			String root = session.getServletContext().getRealPath("/");
+			String pathname = root + "uploads" + File.separator + "Board";
+
+			Board dto = service.readFile(freeFileNum);
+			if (dto != null) {
+				fileManager.doFileDelete(dto.getSaveFilename(), pathname);
+			}
+
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("field", "freeFileNum");
+			map.put("freeNum", freeFileNum);
+			service.deleteFile(map);
+
+			// 작업 결과를 json으로 전송
+			Map<String, Object> model = new HashMap<>();
+			model.put("state", "true");
+			return model;
 		}
 
 
